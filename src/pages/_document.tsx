1e595498d7d4e5019944d7aa9398117574554b00
tecdude <92227828+tecdude@users.noreply.github.com>
2023-02-23 20:17:35 +0000

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class WebsiteDocument extends Document {
 	override render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+				<link rel="icon" type="image/x-icon" href="@images/favicon.ico" />
+				</Head>
 				<body className="bg-[#f7f2f2] dark:bg-gray-800">
 					<Main />
 					<NextScript />
